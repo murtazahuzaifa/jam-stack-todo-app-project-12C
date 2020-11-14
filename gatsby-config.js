acdb9c345ac0919e2cc56cd3eb9d2edac187845a
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2020-11-14 17:41:26 +0500

    project init

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    siteMetadata: {
+        title: `JamStack TodoApp`,
+        description: `Todo app using gatsby, faunadb and netlify serverless function`,
+        author: `Murtaza Hanzala`,
+    },
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-typescript",
+    ]
+}
